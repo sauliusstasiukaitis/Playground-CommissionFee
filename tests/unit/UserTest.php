6c14d46936684d801e0cd834657c111edc5fa3f0
Saulius <saulius.stasiukaitis@gmail.com>
2020-03-07 14:41:46 +0200

    Add test for User class
    To ensure private/company check works.

---
+++ tests/unit/UserTest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+use CommissionFee\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    public function testIsPrivate()
+    {
+        $user = new User(4, USER::USER_TYPE_PRIVATE);
+
+        $this->assertTrue($user->isPrivate(), 'User is private so isPrivate() should return TRUE');
+        $this->assertFalse($user->isCompany(), 'User is private so isCompany() should return FALSE');
+    }
+
+    public function testIsCompany()
+    {
+        $user = new User(4, USER::USER_TYPE_COMPANY);
+
+        $this->assertFalse($user->isPrivate(), 'User is company so isPrivate() should return FALSE');
+        $this->assertTrue($user->isCompany(), 'User is company so isCompany() should return TRUE');
+    }
+}
